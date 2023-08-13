7bb625764768d9f0370e94354bf268e694afbb88
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-08-14 01:06:04 +0200

    Update light.py

--- homeassistant/components/niko_home_control/light.py
+++ homeassistant/components/niko_home_control/light.py
@@ -39,6 +39,7 @@ async def async_setup_entry(
     for action in hub.actions():
         _LOGGER.debug(action.name)
         action_type = Action(action).action_type
+        _LOGGER.debug(action_type)
         if action_type == 1 | action_type == 2:
             entities.append(NikoHomeControlLight(action, hub))
 
