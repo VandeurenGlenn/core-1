3378e84f3fa3972fe1cb1d83fd7fcb65dbeab72e
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-08-14 15:13:09 +0200

    Update cover.py

--- homeassistant/components/niko_home_control/cover.py
+++ homeassistant/components/niko_home_control/cover.py
@@ -71,7 +71,7 @@ class NikoHomeControlCover(CoverEntity):
     async def async_close_cover(self):
         """Close the cover."""
         _LOGGER.debug("Close cover: %s", self.name)
-        self._hub.executeActions(self._cover.id, 127.5)
+        self._hub.executeActions(self._cover.id, 254)
         # self._cover.turn_off()
 
     async def async_open_cover(self):
