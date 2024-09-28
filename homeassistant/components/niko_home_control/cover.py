3ca73098ccff518ba1ca91c815da84eccfa7d29c
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-09-28 02:23:04 +0200

    why state, why don't you update?

--- homeassistant/components/niko_home_control/cover.py
+++ homeassistant/components/niko_home_control/cover.py
@@ -132,24 +132,21 @@ class NikoHomeControlCover(CoverEntity):
         if target > self.current_cover_position:
             self._cover.turn_on(COVER_OPEN)
             while target > self.current_cover_position:
-                await asyncio.sleep(1)
+                await asyncio.sleep(2)
 
         else:
             self._cover.turn_on(COVER_CLOSE)
             while target < self.current_cover_position:
-                await asyncio.sleep(1)
+                await asyncio.sleep(2)
 
         self.stop_cover()
         self._moving = False
 
     def update_state(self, state):
         """Update HA state."""
-        self._cover.update_state(state)
-
-    async def async_added_to_hass(self):
-        """Run when this Entity has been added to HA."""
-        self._cover.register_callback(self.async_write_ha_state)
-
-    async def async_will_remove_from_hass(self):
-        """Entity being removed from hass."""
-        self._cover.remove_callback(self.async_write_ha_state)
+        _LOGGER.debug("Update state: %s", self.name)
+        _LOGGER.debug("State: %s", state)
+        self._cover.state = state
+        self._attr_is_closed = state == 0
+        self._attr_current_cover_position = state
+        self.async_write_ha_state()
